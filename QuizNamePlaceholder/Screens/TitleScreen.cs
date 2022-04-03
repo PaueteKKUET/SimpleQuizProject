10425fb6babebf95790f3b78ef46de918202ecdc
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-04-03 13:25:59 +0200

    Agregar archivos de proyecto.

---
+++ QuizNamePlaceholder/Screens/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuizNamePlaceholder.Screens
+{
+    public partial class TitleScreen : Form
+    {
+        public TitleScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
